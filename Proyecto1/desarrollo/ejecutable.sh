d5fa20cc70c5efe212eec1e56675dc4f0275ccb2
Marito-R-T <mariomoises20008@gmail.com>
2021-09-18 19:14:53 -0600

    septimo commit

--- Proyecto1/desarrollo/ejecutable.sh
+++ Proyecto1/desarrollo/ejecutable.sh
@@ -7,7 +7,7 @@ do
     #ANCHO DE BAJADA
     if [ ${ENLACE[0]} = 'down' ]; then ANCHOB=${ENLACE[1]}; fi
     #ANCHO DE SUBIDA
-    if [ ${ENLACE[0]} = 'up' ]; then ANCHOS=${ENLACE[1]};
+    if [ ${ENLACE[0]} = 'up' ]; then ANCHOS=${ENLACE[1]}; fi
 done < ../archivos/enlace.conf
 ANCHO=$(((ANCHOB+ANCHOS)*1024))
 echo "El ancho de banda es de: ${ANCHO}Kbit"
@@ -36,8 +36,8 @@ do
     BWT=$((ANCHO*(AB+AS)/100))
     readarray -d : -t H_INICIO <<< ${USUARIOBW[3]}
     readarray -d : -t H_FIN <<< ${USUARIOBW[4]}
-    bash ./i_crontab.sh ${H_INICIO[1]} ${H_INICIO[0]} $BWT $CADENA ${USUARIOBW[0]} 
-    bash ./i_crontab.sh ${H_FIN[1]} ${H_FIN[0]} 1 $CADENA ${USUARIOBW[0]}
+    bash ../i_crontab.sh ${H_INICIO[1]} ${H_INICIO[0]} $BWT $CADENA ${USUARIOBW[0]} 
+    bash ../i_crontab.sh ${H_FIN[1]} ${H_FIN[0]} 1 $CADENA ${USUARIOBW[0]}
 done < ../archivos/usuario_BW.conf
 echo
 echo
@@ -53,9 +53,9 @@ do
     if [ ${#USUARIOPROTO[@]} -eq 5 ]; then #parametros: mac,protocolo,puerto(s),horainicio,horafin
       readarray -d : -t PUERTOS <<< "${parametros[2]}"
       if [ ${#PUERTOS[@]} -eq 2 ]; then
-        bash ./i_command.sh 1 ${USUARIOPROTO[1]} ${USUARIOPROTO[0]} ${USUARIOPROTO[3]} ${USUARIOPROTO[4]} ${PUERTOS[0]} ${PUERTOS[1]}
+        bash ../i_command.sh 1 ${USUARIOPROTO[1]} ${USUARIOPROTO[0]} ${USUARIOPROTO[3]} ${USUARIOPROTO[4]} ${PUERTOS[0]} ${PUERTOS[1]}
       else 
-        bash ./i_command.sh 1 ${USUARIOPROTO[1]} ${USUARIOPROTO[0]} ${USUARIOPROTO[3]} ${USUARIOPROTO[4]} ${USUARIOPROTO[2]}
+        bash ../i_command.sh 1 ${USUARIOPROTO[1]} ${USUARIOPROTO[0]} ${USUARIOPROTO[3]} ${USUARIOPROTO[4]} ${USUARIOPROTO[2]}
       fi
     fi
 done < ../archivos/usuario_PROTO.conf
