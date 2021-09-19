e4ad885582522224f4cbfb3c281cacebe273a6cf
Marito-R-T <mariomoises20008@gmail.com>
2021-09-19 13:07:39 -0600

    21vo commit

--- Proyecto1/desarrollo/i_command.sh
+++ Proyecto1/desarrollo/i_command.sh
@@ -29,7 +29,6 @@ if [[ -n $MAC ]]; then
         #iptables -I FORWARD 1 -p $2 -m state --state RELATED,ESTABLISHED -m $2 --sport $6:$7 -m time --timestart $4 --timestop $5 -j ACCEPT
     fi
     if [ $1 -eq 2 ]; then
-if [ $1 -eq 2 ]; then
         echo "echo '/sbin/iptables -I FORWARD 1 -p $2 -m mac --mac-source $MAC -m $2 --dport $6 -j ACCEPT' | at $4" >> /home/marito/Documentos/reglas/reglas.sh
         echo "echo '/sbin/iptables -I FORWARD 1 -p $2 -m state --state RELATED,ESTABLISHED -m $2 --sport $6 -j ACCEPT' | at $4" >> /home/marito/Documentos/reglas/reglas.sh
         echo "echo '/sbin/iptables -D FORWARD -p $2 -m mac --mac-source $MAC -m $2 --dport $6 -j ACCEPT' | at $5" >> /home/marito/Documentos/reglas/reglas.sh
