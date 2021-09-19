3ef308ac8e9db05dfa57f96db52cd60a61e163bb
Marito-R-T <mariomoises20008@gmail.com>
2021-09-18 21:46:09 -0600

    15vo commit

--- Proyecto1/desarrollo/i_crontab.sh
+++ Proyecto1/desarrollo/i_crontab.sh
@@ -1,8 +1,8 @@
 #$1 minuto
 #$2 hora
 #$3 ancho de banda
-#$4 CADENA
-#$5 dirección mac
+#$4 dirección mac
+#$5 CADENA
 
 INTERFAZ=enp0s8
 #direcciones mac de clientes
@@ -17,10 +17,8 @@ done < ../archivos/macsiniciales.conf
 
 if [ $4==$MAC_1 ]; then
     echo "$1 $2 * * * /sbin/tc class change dev $INTERFAZ parent 1:1 classid 1:101 htb rate ${3}Kbit $5" >> /var/spool/cron/crontabs/root
-fi
-if [ $4==$MAC_2 ]; then
+elif [ $4==$MAC_2 ]; then
     echo "$1 $2 * * * /sbin/tc class change dev $INTERFAZ parent 1:1 classid 1:102 htb rate ${3}Kbit $5" >> /var/spool/cron/crontabs/root
-fi
-if [ $4==$MAC_3 ]; then
+elif [ $4==$MAC_3 ]; then
     echo "$1 $2 * * * /sbin/tc class change dev $INTERFAZ parent 1:1 classid 1:103 htb rate ${3}Kbit $5" >> /var/spool/cron/crontabs/root
 fi
