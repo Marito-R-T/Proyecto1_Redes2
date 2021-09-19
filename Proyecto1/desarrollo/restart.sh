e3686b3fd613913d16a75ca856133fce64a852d9
Marito-R-T <mariomoises20008@gmail.com>
2021-09-19 12:05:11 -0600

    19vo commit

--- Proyecto1/desarrollo/restart.sh
+++ Proyecto1/desarrollo/restart.sh
@@ -1,9 +1,18 @@
+#eliminar iptables
 iptables -F 
 
 iptables -P INPUT ACCEPT
 iptables -P OUTPUT ACCEPT
 iptables -P FORWARD ACCEPT
 
+#eliminar los ancho de banda
 tc qdisc del dev enp0s8 root
 
+#eliminar todos los atq
+while read -a l
+do
+    atrm ${l[0]}
+done <<< $(atq)
+
+#eliminar los atq programados por día en el archivo
 cat /dev/null > /home/marito/Documentos/Proyecto1_Redes2/Proyecto1/reglas/reglas.sh
